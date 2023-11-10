a2d3f4c8bd369b1b20e3bc601ba2a531b2d17ac4
chandram13 <mschandr@uci.edu>
2023-11-10 12:54:46 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Marvish Chandra
+
+
+
+def creditCharge(balance,interest,days,paidDays):
+    myBalance = (balance * (interest / 100)) * days
+    payoffTime = myBalance/paidDay
+
+    print("My current credit card cost is about" + " " + myBalance + ".")
+    print("I can pay my credit card debt in" + " " + payoffTime + ".")
+
+
+    creditCharge(1200,26, 60, 30)
